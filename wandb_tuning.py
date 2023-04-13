206ee947476ef936709e98eb051e7d5705019705
SangwonYoon <yoonsw0532@naver.com>
2023-04-13 06:36:05 +0000

    add document

--- wandb_tuning.py
+++ wandb_tuning.py
@@ -16,13 +16,16 @@ from scipy.stats import pearsonr
 import wandb
 
 def seed_everything(seed):
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+    random.seed(seed) # Python의 random 모듈의 시드를 설정
+    os.environ['PYTHONHASHSEED'] = str(seed) # Python 해시 함수의 시드를 설정
+    np.random.seed(seed) # NumPy의 무작위 함수인 random 모듈의 시드를 설정
+    torch.manual_seed(seed) # PyTorch의 무작위 함수를 위한 시드를 설정
+    torch.cuda.manual_seed(seed) # PyTorch의 CUDA(Compute Unified Device Architecture) 연산을 위한 시드를 설정
+    torch.backends.cudnn.deterministic = True # PyTorch의 cuDNN(CUDA Deep Neural Network) 라이브러리의 동작을 재현 가능하게 만들기 위한 옵션을 설정
+    # PyTorch의 cuDNN 라이브러리가 입력 데이터의 크기에 따라 최적의 컨볼루션 연산 알고리즘을 선택하는 옵션을 설정
+    # True로 설정하면 동일한 입력 데이터에 대해서도 항상 같은 알고리즘이 선택되어, 동일한 결과를 재현할 수 있다.
     torch.backends.cudnn.benchmark = True
+
 def compute_pearson_correlation(pred):
     preds = pred.predictions.flatten()
     labels = pred.label_ids.flatten()
@@ -30,6 +33,16 @@ def compute_pearson_correlation(pred):
 
 class Train_val_TextDataset(torch.utils.data.Dataset):
     def __init__(self, data_file, text_columns, target_columns=None, delete_columns=None, max_length=512, model_name='klue/roberta-small'):
+        """
+        클래스의 생성자 메서드로, 데이터셋을 초기화한다.
+        Args:
+            data_file (string): 데이터 파일의 경로
+            text_columns (list): 텍스트 데이터를 갖는 column들의 이름
+            target_columns (string or list, optional): 레이블 데이터를 갖는 column의 이름
+            delete_columns (string or list, optional): 제거할 column의 이름
+            max_length (int, optional): 최대 텍스트 길이
+            model_name ()
+        """
         self.data = pd.read_csv(data_file)
         self.text_columns = text_columns
         self.target_columns = target_columns if target_columns is not None else []
@@ -66,7 +79,7 @@ class Train_val_TextDataset(torch.utils.data.Dataset):
 
 if __name__ == '__main__':
     seed_everything(42)
-    wandb.login(key='38e2b6604d2670c05fd7f22edb2a711faf495709')
+    wandb.login(key='c0d96b72557660bd63642b07a905e93575f72573')
 
     sweep_config = {
         'method': 'random'
