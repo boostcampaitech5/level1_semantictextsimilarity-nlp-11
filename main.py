330ac77f80476157f10741be587450e3c8b5cc40
MinJae Kang <kminjae618@gmail.com>
2023-04-15 08:55:00 +0000

    [score 0.9293] koelectra-base-v3-discriminator / raw data #41

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 
 import pandas as pd
-from soynlp.normalizer import repeat_normalize
-from soynlp.tokenizer import RegexTokenizer
 from tqdm.auto import tqdm
 import transformers
 import torch
@@ -15,11 +13,10 @@ from transformers import AutoTokenizer
 import numpy as np
 from scipy.stats import pearsonr
 import random
-import nltk
-from nltk.corpus import stopwords
+from datetime import datetime
 
-stopwords = pd.read_csv('./data/stopwords.csv',encoding='cp949')
-Regextokenizer = RegexTokenizer()
+#import nltk
+# from nltk.corpus import stopwords
 def compute_pearson_correlation(pred):
     preds = pred.predictions.flatten()
     labels = pred.label_ids.flatten()
@@ -39,8 +36,7 @@ def seed_everything(seed):
 
 class Train_val_TextDataset(torch.utils.data.Dataset):
     def __init__(self,state,data_file, text_columns, target_columns=None, delete_columns=None, max_length=512, model_name='klue/roberta-small'):
-        self.state = state
-        if self.state == 'train':
+        if state == 'train':
             self.data = pd.read_csv(data_file)
             #self.add_data = pd.read_csv('./data/preprocessed_data_sin_v2_filter.csv')
             #self.data = pd.concat([self.data,self.add_data])
@@ -52,25 +48,12 @@ class Train_val_TextDataset(torch.utils.data.Dataset):
         self.max_length = max_length
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.inputs, self.targets = self.preprocessing(self.data)
-        self.stopwords = pd.read_csv('./data/stopwords.csv', encoding='cp949')
 
     def __getitem__(self, idx):
         if len(self.targets) == 0:
             return torch.tensor(self.inputs[idx])
         else:
-            if self.state=='train':
-                target_val = self.targets[idx]
-                if self.delete_columns is not None and self.data.iloc[idx][self.delete_columns] == 1:
-                    if random.random() <= 0.2:
-                        target_val += random.uniform(0.0, 0.1)
-                else:
-                    if random.random() <= 0.2:
-                        target_val -= random.uniform(0.0, 0.1)
-
-                target_val = max(min(target_val, 5.0), 0.0)
-                return {"input_ids": torch.tensor(self.inputs[idx]), "labels": torch.tensor(target_val)}
-            else:
-                return {"input_ids": torch.tensor(self.inputs[idx]), "labels": torch.tensor(self.targets[idx])}
+            return {"input_ids": torch.tensor(self.inputs[idx]), "labels": torch.tensor(self.targets[idx])}
 
     def __len__(self):
         return len(self.inputs)
@@ -83,8 +66,9 @@ class Train_val_TextDataset(torch.utils.data.Dataset):
     def tokenizing(self, dataframe):
         data = []
         for idx, item in tqdm(dataframe.iterrows(), desc='Tokenizing', total=len(dataframe)):
-            text = '[SEP]'.join([self.preprocess_text(item[text_column]) for text_column in self.text_columns])
 
+            text = '[SEP]'.join([item[text_column] for text_column in self.text_columns])
+            ##불용어 제거
             outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True,
                                      max_length=self.max_length)
             data.append(outputs['input_ids'])
@@ -99,46 +83,41 @@ class Train_val_TextDataset(torch.utils.data.Dataset):
         inputs = self.tokenizing(data)
         return inputs, targets
 
-    def preprocess_text(self,text):
-        # normalize repeated characters using soynlp library
-        text = repeat_normalize(text, num_repeats=2)
-        # remove stopwords
-        #text = ' '.join([token for token in text.split() if not token in stopwords])
-        # remove special characters and numbers
-        # text = re.sub('[^가-힣 ]', '', text)
-        # text = re.sub('[^a-zA-Zㄱ-ㅎ가-힣]', '', text)
-        # tokenize text using soynlp tokenizer
-        tokens = Regextokenizer.tokenize(text)
-        # lowercase all tokens
-        tokens = [token.lower() for token in tokens]
-        # join tokens back into sentence
-        text = ' '.join(tokens)
-        # kospacing_sent = spacing(text)
-        return text
+
 
 if __name__ == '__main__':
 
-    seed_everything(43)
-    model = AutoModelForSequenceClassification.from_pretrained("monologg/koelectra-base-v3-discriminator",num_labels=1,ignore_mismatched_sizes=True)
-    #model = AutoModelForSequenceClassification.from_pretrained("E:/nlp/checkpoint/best_acc/checkpoint-16317",num_labels=1,ignore_mismatched_sizes=True)
+    seed_everything(42)
+    #model = AutoModelForSequenceClassification.from_pretrained("lighthouse/mdeberta-v3-base-kor-further",num_labels=1,ignore_mismatched_sizes=True)
+    
+    model_name = "monologg/koelectra-base-v3-discriminator"
+    train_data_name = './data/train.csv'
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name,num_labels=1,ignore_mismatched_sizes=True)
+    # model = AutoModelForSequenceClassification.from_pretrained('E:/nlp/checkpoint/TEST-6/checkpoint-4081', num_labels=1, ignore_mismatched_sizes=True)
+
+    max_length = 256  # 원래 512
 
 
-    Train_textDataset = Train_val_TextDataset('train','./data/train.csv',['sentence_1', 'sentence_2'],'label','binary-label',max_length=512,model_name="monologg/koelectra-base-v3-discriminator")
-    Val_textDataset = Train_val_TextDataset('val','./data/dev.csv',['sentence_1', 'sentence_2'],'label','binary-label',max_length=512,model_name="monologg/koelectra-base-v3-discriminator")
+
+    Train_textDataset = Train_val_TextDataset('train', train_data_name ,['sentence_1', 'sentence_2'],'label','binary-label',max_length=max_length,model_name=model_name)
+    Val_textDataset = Train_val_TextDataset('val','./data/dev.csv',['sentence_1', 'sentence_2'],'label','binary-label',max_length=max_length,model_name=model_name)
 
     args = TrainingArguments(
-        "E:/nlp/checkpoint/best_acc_/koelectra-labelsoomthing_0.4_0.1_0.2",
+        f'checkpoint/{model_name}/{train_data_name}/{datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")}',
         evaluation_strategy = "epoch",
         save_strategy = "epoch",
-        learning_rate=0.00002860270719188072, #0.000005
+        learning_rate=2.860270719188072e-05, #0.000005
+        # group_by_length=True,
+        # auto_find_batch_size=True,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
-        num_train_epochs=10,
+        num_train_epochs=7,
         weight_decay=0.5,
         load_best_model_at_end=True,
         dataloader_num_workers = 4,
         logging_steps=200,
-        seed = 43
+        seed = 42
     )
 
     trainer = Trainer(
--- wandb_tuning.py
+++ wandb_tuning.py
@@ -1,5 +1,3 @@
-import os
-
 import pandas as pd
 from tqdm.auto import tqdm
 import transformers
@@ -13,14 +11,9 @@ from transformers import AutoTokenizer
 import numpy as np
 from scipy.stats import pearsonr
 import random
+import os
 from datetime import datetime
-
-#import nltk
-# from nltk.corpus import stopwords
-def compute_pearson_correlation(pred):
-    preds = pred.predictions.flatten()
-    labels = pred.label_ids.flatten()
-    return {"pearson_correlation": pearsonr(preds, labels)[0]}
+import wandb
 
 
 def seed_everything(seed):
@@ -32,16 +25,14 @@ def seed_everything(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
 
-
+def compute_pearson_correlation(pred):
+    preds = pred.predictions.flatten()
+    labels = pred.label_ids.flatten()
+    return {"pearson_correlation": pearsonr(preds, labels)[0]}
 
 class Train_val_TextDataset(torch.utils.data.Dataset):
-    def __init__(self,state,data_file, text_columns, target_columns=None, delete_columns=None, max_length=512, model_name='klue/roberta-small'):
-        if state == 'train':
-            self.data = pd.read_csv(data_file)
-            #self.add_data = pd.read_csv('./data/preprocessed_data_sin_v2_filter.csv')
-            #self.data = pd.concat([self.data,self.add_data])
-        else:
-            self.data = pd.read_csv(data_file)
+    def __init__(self, data_file, text_columns, target_columns=None, delete_columns=None, max_length=512, model_name='klue/roberta-small'):
+        self.data = pd.read_csv(data_file)
         self.text_columns = text_columns
         self.target_columns = target_columns if target_columns is not None else []
         self.delete_columns = delete_columns if delete_columns is not None else []
@@ -58,19 +49,11 @@ class Train_val_TextDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.inputs)
 
-    def remove_stopwords(self, text):
-        words = text.split()
-        words = [word for word in words if word not in stopwords]
-        return ' '.join(words)
-
     def tokenizing(self, dataframe):
-        data = []
+        data=[]
         for idx, item in tqdm(dataframe.iterrows(), desc='Tokenizing', total=len(dataframe)):
-
             text = '[SEP]'.join([item[text_column] for text_column in self.text_columns])
-            ##불용어 제거
-            outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True,
-                                     max_length=self.max_length)
+            outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True, max_length=self.max_length)
             data.append(outputs['input_ids'])
         return data
 
@@ -83,50 +66,81 @@ class Train_val_TextDataset(torch.utils.data.Dataset):
         inputs = self.tokenizing(data)
         return inputs, targets
 
-
-
 if __name__ == '__main__':
-
     seed_everything(42)
-    #model = AutoModelForSequenceClassification.from_pretrained("lighthouse/mdeberta-v3-base-kor-further",num_labels=1,ignore_mismatched_sizes=True)
-    
+    wandb.login(key='37d25d8bc75df6ee575ed47618c92c60d5006c17')
+
+    sweep_config = {
+        'method': 'random'
+    }
+
+    # hyperparameters
+    parameters_dict = {
+        'epochs': {
+            'values': [7, 8, 9, 10, 11, 12]
+        },
+        'batch_size': {
+            'values': [8, 16, 32, 64]
+        },
+        'learning_rate': {
+            'distribution': 'log_uniform_values',
+            'min': 2e-5, # 0.00002
+            'max': 3e-5  # 0.00003
+                         # 0.00002860270719188072
+        },
+        'weight_decay': {
+            'values': [0.1, 0.2, 0.3, 0.4, 0.5]
+        },
+    }
+    sweep_config['parameters'] = parameters_dict
+    sweep_id = wandb.sweep(sweep_config, project="discriminator_param_sweep")
+
     model_name = "monologg/koelectra-base-v3-discriminator"
-    train_data_name = './data/train.csv'
-
+    train_data_name = 'train.csv'
+    max_length = 256  # 512
+    
     model = AutoModelForSequenceClassification.from_pretrained(model_name,num_labels=1,ignore_mismatched_sizes=True)
-    # model = AutoModelForSequenceClassification.from_pretrained('E:/nlp/checkpoint/TEST-6/checkpoint-4081', num_labels=1, ignore_mismatched_sizes=True)
 
-    max_length = 256  # 원래 512
+    #model = transformers.AutoModelForSequenceClassification.from_pretrained(
+    #   'C:/Users/tm011/PycharmProjects/NLP_COMP/checkpoint/checkpoint-6993')
+    Train_textDataset = Train_val_TextDataset(f'./data/{train_data_name}',['sentence_1', 'sentence_2'],'label','binary-label',max_length=max_length,model_name=model_name)
+    Val_textDataset = Train_val_TextDataset('./data/dev.csv',['sentence_1', 'sentence_2'],'label','binary-label',max_length=max_length,model_name=model_name)
 
 
+    def model_init():
+        model = AutoModelForSequenceClassification.from_pretrained(model_name,num_labels=1,ignore_mismatched_sizes=True)
+        return model
 
-    Train_textDataset = Train_val_TextDataset('train', train_data_name ,['sentence_1', 'sentence_2'],'label','binary-label',max_length=max_length,model_name=model_name)
-    Val_textDataset = Train_val_TextDataset('val','./data/dev.csv',['sentence_1', 'sentence_2'],'label','binary-label',max_length=max_length,model_name=model_name)
 
-    args = TrainingArguments(
-        f'checkpoint/{model_name}/{train_data_name}/{datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")}',
-        evaluation_strategy = "epoch",
-        save_strategy = "epoch",
-        learning_rate=2.860270719188072e-05, #0.000005
-        # group_by_length=True,
-        # auto_find_batch_size=True,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
-        num_train_epochs=7,
-        weight_decay=0.5,
-        load_best_model_at_end=True,
-        dataloader_num_workers = 4,
-        logging_steps=200,
-        seed = 42
-    )
+    def train(config=None):
+        with wandb.init(config=config):
+            config = wandb.config
+            t = config.learning_rate
+            args = TrainingArguments(
+                f'param_sweep/checkpoint/{model_name}/{train_data_name}/{datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")}',
+                evaluation_strategy="epoch",
+                save_strategy="epoch",
+                report_to='wandb',
+                learning_rate=config.learning_rate,
+                per_device_train_batch_size=config.batch_size,
+                per_device_eval_batch_size=config.batch_size,
+                num_train_epochs=config.epochs,
+                weight_decay=config.weight_decay,
+                load_best_model_at_end=True,
+                dataloader_num_workers=4,
+                logging_steps=200,
+                seed=42
+            )
+            trainer = Trainer(
+                model_init = model_init,
+                args=args,
+                train_dataset=Train_textDataset,
+                eval_dataset=Val_textDataset,
+                # tokenizer=tokenizer,
+                compute_metrics=compute_pearson_correlation
+            )
 
-    trainer = Trainer(
-        model,
-        args,
-        train_dataset=Train_textDataset,
-        eval_dataset=Val_textDataset,
-        #tokenizer=tokenizer,
-        compute_metrics=compute_pearson_correlation
-    )
+            trainer.train()
 
-    trainer.train()
+
+    wandb.agent(sweep_id, train, count=15)
