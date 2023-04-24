85d197854e01cf3fe9669560fcfeb1b5a819b272
Kim Juwon <kjwt1124@icloud.com>
2023-04-24 18:36:12 +0900

    Update DataLoader

--- dataloader.py
+++ dataloader.py
@@ -1,1 +1,40 @@
+from transformers import AutoTokenizer
+from tqdm.auto import tqdm
 import torch
+
+class Dataset(torch.utils.data.Dataset):
+    
+    def __init__(self,data_file, text_columns, target_columns=None, delete_columns=None, max_length=512, model_name='klue/roberta-small'):
+        self.data = pd.read_csv(data_file)
+        self.text_columns = text_columns
+        self.target_columns = target_columns if target_columns is not None else []
+        self.delete_columns = delete_columns if delete_columns is not None else []
+        self.max_length = max_length
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.inputs, self.targets = self.preprocessing(self.data)
+
+    def __getitem__(self, idx):
+        if len(self.targets) == 0:
+            return torch.tensor(self.inputs[idx])
+        else:
+            return {'input_ids': torch.tensor(self.inputs[idx]), 'labels': torch.tensor(self.targets[idx])}
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def tokenizing(self, dataframe):
+        data=[]
+        for _, item in tqdm(dataframe.iterrows(), desc='Tokenizing', total=len(dataframe)):
+            text = '[SEP]'.join([item[text_column] for text_column in self.text_columns])
+            outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True, max_length=self.max_length)
+            data.append(outputs['input_ids'])
+        return data
+
+    def preprocessing(self, data):
+        data = data.drop(columns=self.delete_columns)
+        try:
+            targets = data[self.target_columns].values.tolist()
+        except:
+            targets = []
+        inputs = self.tokenizing(data)
+        return inputs, targets
