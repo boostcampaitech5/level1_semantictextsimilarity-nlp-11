82e931f12d7e3c63fe77e730700f074b4683978c
Kim Juwon <kjwt1124@icloud.com>
2023-04-24 19:10:17 +0900

    Update train.py

---
+++ train.py
@@ -0,0 +1,53 @@
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+from utils import compute_pearson_correlation, seed_everything
+from augmentation import augment
+from dataloader import Dataset
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+import os
+from utils import load_yaml
+
+
+prj_dir = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == '__main__':
+
+    config_path = os.path.join(prj_dir, 'config_yaml', 'kykim.yaml')
+    config = load_yaml(config_path)
+
+    if (os.path.isfile(config['aug_data_folder']['Train_data'])) == False:
+        augment()
+
+    model = AutoModelForSequenceClassification.from_pretrained(config['architecture'], num_labels=1,ignore_mismatched_sizes=True)
+
+    Train_textDataset = Dataset(config['aug_data_folder']['Train_data'], 'train', ['sentence_1', 'sentence_2'], 'label','binary-label', max_length=256,model_name=config['architecture'])
+    Val_textDataset = Dataset(config['aug_data_folder']['Val_data'], 'val', ['sentence_1', 'sentence_2'], 'label','binary-label', max_length=256,model_name=config['architecture'])
+
+    args = TrainingArguments(
+        os.path.join(prj_dir,'Save_folder',config['name']),
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        learning_rate=config['lr'],
+        per_device_train_batch_size=config['batch_size'],
+        per_device_eval_batch_size=config['batch_size'],
+        num_train_epochs=config['n_epochs'],
+        weight_decay=config['weight_decay'],
+        load_best_model_at_end=True,
+        dataloader_num_workers=4,
+        logging_steps=100,
+        seed=config['seed'],
+        group_by_length=True,
+        lr_scheduler_type = config['scheduler']
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=Train_textDataset,
+        eval_dataset=Val_textDataset,
+        compute_metrics=compute_pearson_correlation,
+    )
+
+    trainer.train()
