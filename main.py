a1e74a22f7bdee0e126acd26ecd30bba566b481d
MinJae Kang <kminjae618@gmail.com>
2023-04-24 19:05:53 +0900

    Edit utils.py and main.py

--- main.py
+++ main.py
@@ -1,75 +1,32 @@
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
-from tqdm.auto import tqdm
 from utils import compute_pearson_correlation, seed_everything
 from dataloader import Dataset
-import pandas as pd
 import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# class Train_val_TextDataset(torch.utils.data.Dataset):
-#     def __init__(self,data_file, state,text_columns, target_columns=None, delete_columns=None, max_length=512, model_name='klue/roberta-small'):
-#         self.state = state
-#         if self.state == 'train':
-#             self.data = pd.read_csv(data_file)
-#         else:
-#             self.data = pd.read_csv(data_file)
-#         self.text_columns = text_columns
-#         self.target_columns = target_columns if target_columns is not None else []
-#         self.delete_columns = delete_columns if delete_columns is not None else []
-#         self.max_length = max_length
-#         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-#         self.inputs, self.targets = self.preprocessing(self.data)
-
-#     def __getitem__(self, idx):
-#         if len(self.targets) == 0:
-#             return torch.tensor(self.inputs[idx])
-#         else:
-#             return {"input_ids": torch.tensor(self.inputs[idx]), "labels": torch.tensor(self.targets[idx])}
-
-#     def __len__(self):
-#         return len(self.inputs)
-
-#     def tokenizing(self, dataframe):
-#         data=[]
-#         for idx, item in tqdm(dataframe.iterrows(), desc='Tokenizing', total=len(dataframe)):
-#             text = '[SEP]'.join([item[text_column] for text_column in self.text_columns])
-#             outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True, max_length=self.max_length)
-#             data.append(outputs['input_ids'])
-#         return data
-
-#     def preprocessing(self, data):
-#         data = data.drop(columns=self.delete_columns)
-#         try:
-#             targets = data[self.target_columns].values.tolist()
-#         except:
-#             targets = []
-#         inputs = self.tokenizing(data)
-#         return inputs, targets
-
 
 
 if __name__ == '__main__':
 
     seed_everything(43)
-    model_name = "kykim/electra-kor-base"
+    model_name = 'kykim/electra-kor-base'
     model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=1,
                                                                ignore_mismatched_sizes=True)
 
 
 
-    Train_textDataset = Dataset('./data/best_data_v1.csv', 'train', ['sentence_1', 'sentence_2'], 'label',
+    Train_textDataset = Dataset('./data/best_data_v1.csv', ['sentence_1', 'sentence_2'], 'label',
                                               'binary-label', max_length=256,
                                               model_name=model_name)
-    Val_textDataset = Dataset('./data/dev.csv', 'val', ['sentence_1', 'sentence_2'], 'label',
+    Val_textDataset = Dataset('./data/dev.csv', ['sentence_1', 'sentence_2'], 'label',
                                             'binary-label', max_length=256,
                                             model_name=model_name)
 
     args = TrainingArguments(
-        "E:/nlp/checkpoint/elector/electra-kor-base_jehyun__RE_RE",
-        evaluation_strategy = "epoch",
-        save_strategy = "epoch",
-        learning_rate=0.00002071889728509824, #0.000005
+        'E:/nlp/checkpoint/elector/electra-kor-base_jehyun__RE_RE',
+        evaluation_strategy = 'epoch',
+        save_strategy = 'epoch',
+        learning_rate=0.00002071889728509824,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
         num_train_epochs=10,
--- utils.py
+++ utils.py
@@ -1,16 +1,31 @@
 from scipy.stats import pearsonr
 import os
 import random
+import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
-import numpy as np
 
-def compute_pearson_correlation(pred):
+def compute_pearson_correlation(pred: torch.tensor) -> dict:
+    """
+    피어슨 상관 계수를 계산해주는 함수
+        Args:
+            pred (torch.tensor): 모델의 예측값과 레이블을 포함한 데이터
+        Returns:
+            perason_correlation (dict): 입력값을 통해 계산한 피어슨 상관 계수
+    """
     preds = pred.predictions.flatten()
     labels = pred.label_ids.flatten()
-    return {"pearson_correlation": pearsonr(preds, labels)[0]}
+    perason_correlation = {"pearson_correlation": pearsonr(preds, labels)[0]}
+    return perason_correlation
 
-def seed_everything(seed):
+def seed_everything(seed: int) -> None:
+    """
+    모델에서 사용하는 모든 랜덤 시드를 고정해주는 함수
+        Args:
+            seed (int): 시드 고정에 사용할 정수값
+        Returns:
+            None
+    """
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
